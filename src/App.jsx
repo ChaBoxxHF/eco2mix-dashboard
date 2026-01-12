c5b2a4240b5ba1783f8e51b84435bf3c43717188
Pascal 'ChaBoxxHF' CROCHARD <pascal.maniak@gmail.com>
2026-01-12 14:33:34 +0100

    Utilisation des props et premier hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import InfosPanel from "./components/InfosPanel/InfoPanel";
+import MetropolisCard from "./components/MetropolisCard/MetropolisCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <InfosPanel /*totalCities={15}*/ avgConsommation={2222} period={"test"} />
+      <MetropolisCard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MetropolisCard/MetropolisCard.jsx
+++ src/components/MetropolisCard/MetropolisCard.jsx
@@ -1,20 +1,26 @@
 import "./MetropolisCard.css";
-import myData from "/eco2mix-dashboard//public/json/eco2mix-metropoles-2025-janvier-01.json";
+import myData from "/public/json/eco2mix-metropoles-2025-janvier-01.json";
 
 function MetropolisCard() {
-    const liste = myData.map(() =>)
   const ville = "Paris";
   const consommation = "1200 MW";
+  const villes = myData;
+  console.log(villes);
   return (
-    <div className="metropolis-card">
-      <h2>{ville}</h2>
-      <div className="card-content">
-        <p>
-          <span className="label">Consommation :</span>
-          <span className="value">{consommation}</span>
-        </p>
+    <>
+      <div className="metropolis-card">
+        <h2>nbVilles = {myData.length}</h2>
+        <div className="card-content">
+          <p>
+            <span className="label">Consommation :</span>
+            <span className="value">{consommation}</span>
+          </p>
+        </div>
       </div>
-    </div>
+      {/* <div>{myData.map((ville)=>
+        
+      )}</div> */}
+    </>
   );
 }
 
