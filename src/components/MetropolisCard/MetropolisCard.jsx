29b3402157d1c55d315c894d695a83210e814287
Pascal 'ChaBoxxHF' CROCHARD <pascal.maniak@gmail.com>
2026-01-12 11:29:02 +0100

    fin de la partie solo (il faudrait du css, ça arrive)

---
+++ src/components/MetropolisCard/MetropolisCard.jsx
@@ -0,0 +1,17 @@
+function MetropolisCard() {
+  const ville = "Paris";
+  const consommation = "1200 MW";
+  return (
+    <div className="metropolis-card">
+      <h2>{ville}</h2>
+      <div className="card-content">
+        <p>
+          <span className="label">Consommation :</span>
+          <span className="value">{consommation}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MetropolisCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import InfosPanel from "./components/InfosPanel/InfoPanel.jsx";
+import MetropolisCard from "./components/MetropolisCard/MetropolisCard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <MetropolisCard />
     <InfosPanel />
   </StrictMode>
 );
